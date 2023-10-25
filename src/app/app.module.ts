2be6244cff6ac2d6cbcbb83c0eedbe5ddb15760a
KT <krakovszkyt@gmail.com>
2023-10-25 22:04:03 +0200

    Add Garden, Plant, Navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GardenComponent } from './components/garden/garden.component';
+import { PlantComponent } from './components/plant/plant.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GardenComponent,
+    PlantComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
