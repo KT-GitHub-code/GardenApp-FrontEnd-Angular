e9b1ff04ad4f9b8ab2ba5aca1b63e0510393322c
KT <krakovszkyt@gmail.com>
2023-11-11 20:27:55 +0100

    Add Plant posting

--- src/app/components/new-plant-card/new-plant-card.component.ts
+++ src/app/components/new-plant-card/new-plant-card.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {GardenService} from "../../services/garden.service";
 
 @Component({
   selector: 'app-new-plant-card',
@@ -10,4 +13,23 @@ export class NewPlantCardComponent {
   @Input()
   plantType: string;
 
+  token = localStorage.getItem('token');
+
+  constructor(private http: HttpClient,
+              private router: Router,
+              private gardenService: GardenService) {
+  }
+
+  createPlant() {
+    this.http.post('http://localhost:9000/api/plant',
+      {
+        "type": this.plantType,
+        "gardenId": this.gardenService.garden.id
+      },
+      {headers: { Authorization: 'Bearer '+ this.token,'Content-Type': 'application/json' }, withCredentials: true}
+    )
+      .subscribe((res)=>{console.log(res);});
+    this.router.navigate(['Home']);
+
+  }
 }
